ce2d45892f81b93f94075662534648bf8cf35f36
toureydaou <ttoureydaou@gmail.com>
2023-04-24 10:00:48 +0000

    ajout du gitignore et des configs pour postgresql

--- projet_ifnti/settings.py
+++ projet_ifnti/settings.py
@@ -78,6 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'ifnti_db',
+        # 'USER': 'ifnti',
+        # 'PASSWORD': 'ifnti',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
