705c1ae777d57c90986d2e8f88a8edbdbb8398d8
amk <abdoulmalikkondi8@gmail.com>
2023-04-30 18:04:38 +0000

    task: crud model Note, adding file models_forms.py updating main/url.py

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('liste_des_etudiants', views.createEtudiant, name='liste_des_etudiants'),
+    path('add_note/<int:id_edudiant>/<int:id_matiere>/<int:id_semestre>', views.createNote, name='add-note'),
+    path('edit_note/<int:id>', views.editNote, name='edit-note'),
+    path('delete_note/<int:id>', views.deleteNote, name='delete-note'),
 ]
